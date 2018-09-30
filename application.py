f8973a803175d1e5ccf791531ac2cc5a842002e3
raphaeluziel <raphaeluziel@hotmail.com>
2018-09-29 20:40:38 -0400

    and or fix

--- application.py
+++ application.py
@@ -94,7 +94,7 @@ def login():
 
         # Ensure username exists and password is correct
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return errorPage("invalid username and%2for password", 403)
+            return errorPage("invalid username and or password", 403)
 
         # Remember which user has logged in
         session["user_id"] = rows[0]["user_id"]
